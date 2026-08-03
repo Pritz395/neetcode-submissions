eaa5c315838ce2ff304f07e621ac64ed5c4ab55b
preetham <preethampujari395@gmail.com>
2026-08-03 21:40:33 +0530

    Add: buy-and-sell-crypto - submission-8

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-8.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maximum=0
+        minimum=prices[0]
+
+        for sell in prices:
+            maximum=max(maximum,sell-minimum)
+            minimum=min(minimum,sell)
+        return maximum
