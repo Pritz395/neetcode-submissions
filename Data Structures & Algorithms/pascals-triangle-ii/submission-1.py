d096dc5fc67872c8125fbf8c87a97166e0b02934
preetham <preethampujari395@gmail.com>
2026-08-06 21:08:51 +0530

    Add: pascals-triangle-ii - submission-1

---
+++ Data Structures & Algorithms/pascals-triangle-ii/submission-1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        res = [1]
+
+        for i in range(rowIndex):
+            res = [1] + [res[j] + res[j+1] for j in range(len(res)-1)] + [1]
+
+        return res
