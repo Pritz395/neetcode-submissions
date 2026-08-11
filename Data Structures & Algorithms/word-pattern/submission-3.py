d7b24bc854cf120a9539818f156342cf08188e35
preetham <preethampujari395@gmail.com>
2026-08-11 20:49:33 +0530

    Add: word-pattern - submission-3

---
+++ Data Structures & Algorithms/word-pattern/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        s=s.split(" ")
+        if len(pattern)!=len(s):
+            return False
+
+        charmap={}
+        wordmap={}
+
+        for c,w in zip(pattern,s):
+            if c in charmap and charmap[c]!=w:
+                return False
+            if w in wordmap and wordmap[w]!=c:
+                return False
+        
+            charmap[c]=w
+            wordmap[w]=c
+
+        return True
